297fbad4c7dac9f8bf58814e19160cf91d558fdc
tyson983 <93043435+tyson983@users.noreply.github.com>
2025-02-19 15:41:00 -0500

    Add files via upload

---
+++ Run-command2.ps1
@@ -0,0 +1,28 @@
+﻿# Run a command
+function RunCommand([ScriptBlock] $command) {
+
+    # Run the command and write the output to the window and to a variable ("SFC" formatting)
+    $stringcommand = $command.ToString()
+    if (
+        $stringcommand -match "^SFC$" -or
+        $stringcommand -match "^SFC.exe$" -or
+        $stringcommand -match "^SFC .*$" -or
+        $stringcommand -match "^SFC.exe .*$"
+    ) {
+        $oldEncoding = [console]::OutputEncoding
+        [console]::OutputEncoding = [Text.Encoding]::Unicode
+        $command = [ScriptBlock]::Create("(" + $stringcommand + ")" + " -join ""`r`n"" -replace ""`r`n`r`n"", ""`r`n""")
+        & ($command) 2>&1 | Tee-Object -Variable out_content
+        [console]::OutputEncoding = $oldEncoding
+
+    # Run the command and write the output to the window and to a variable (normal formatting)
+    } else {
+        & ($command) 2>&1 | Tee-Object -Variable out_content
+    }
+
+    # Manipulate output variable, write it to a file...
+    # ...
+    return
+}
+
+RunCommand {Sfc /verifyonly}
---
+++ WMIDriverinstall.ps1
@@ -0,0 +1,5 @@
+﻿
+# Listing device by name
+Get-WmiObject Win32_PnPSignedDriver| select devicename| where {$_.devicename -like "Intel(R) Connectivity Performance Suite"}
+# Deleting a device by name
+(Get-WmiObject Win32_PnPSignedDriver -filter "DeviceName='Intel(R) Connectivity Performance Suite'").delete()
